24566bff91feeafed2e1b0d3b2944e872be3eca5
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-10-22 15:37:04 +0200

    snack-1 solved

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,12 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+let result = '';
+names.forEach((name, i) => {
+    result += `'${name}'`;
+    if (i < names.length - 1) {
+        result += ', ';
+    }
+});
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+console.log(result);
